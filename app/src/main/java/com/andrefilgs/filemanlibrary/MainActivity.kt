dd3126f220f0a10c4b4b65009039198623cd6bc7
andrezizu <alg.filgueiras@gmail.com>
2020-05-26 16:55:42 -0300

    simple version

--- app/src/main/java/com/andrefilgs/filemanlibrary/MainActivity.kt
+++ app/src/main/java/com/andrefilgs/filemanlibrary/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.andrefilgs.filemanlibrary
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.andrefilgs.fileman.Fileman
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
+//https://medium.com/@elizarov/the-reason-to-avoid-globalscope-835337445abc
 class MainActivity : AppCompatActivity(), CoroutineScope {
 
     override val coroutineContext: CoroutineContext = Dispatchers.Main
@@ -31,7 +32,7 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
         cancel()
     }
 
-    @ExperimentalCoroutinesApi
+
     private fun setClickListeners() {
 
         //This way blocks the UI
@@ -52,17 +53,17 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
                 val res = withContext(Dispatchers.IO) {
                     writePerson(createDummyPerson())
                 }
-                Dispatchers.Main{
-                    tv_sample.text = if (res) "File written with success" else "File have not been written"
+                Dispatchers.Main {
+                    tv_sample.text =
+                        if (res) "File written with success" else "File have not been written"
                 }
             }
         }
 
 
-
         btn_sample_read.setOnClickListener {
-            GlobalScope.launch {
-                //        val res = async {  coroReadPerson()}.await()
+
+            launch(Dispatchers.Default){
                 val res = withContext(Dispatchers.IO) { readPerson() }
                 withContext(context = Dispatchers.Main){
                     tv_sample.text = res ?: "File does not exist"
@@ -70,7 +71,42 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
             }
             tv_sample.text = "reading..."
 
+
+//            GlobalScope.launch {
+//                //        val res = async {  coroReadPerson()}.await()
+//                val res = withContext(Dispatchers.IO) { readPerson() }
+//                withContext(context = Dispatchers.Main){
+//                    tv_sample.text = res ?: "File does not exist"
+//                }
+//            }
+//            tv_sample.text = "reading..."
+
         }
+
+
+        //Simple Blocking Coroutine example
+        btn_sample_sync_coro.setOnClickListener {
+
+            launch(Dispatchers.IO){
+                val response = checkValue(2)
+                withContext(Dispatchers.Main){
+                    tv_sample.text = response
+                }
+            }
+            tv_sample.text = "reading Sync test..."
+        }
+
+    }
+
+    private fun checkValue(value: Int): String {
+        return "$value is greater than 2?\nResult = ${runBlocking {
+            syncCoroutine(value)
+        }}"
+    }
+
+    suspend fun syncCoroutine(sampleText: Int): Boolean = coroutineScope {
+        Thread.sleep(3000) //just to show the power of coroutine
+        sampleText > 2
     }
 
 
@@ -81,7 +117,15 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
 
     private fun writePerson(person: Person): Boolean {
         val jsonString = Gson().toJson(person, Person::class.java)
-        return Fileman.write(jsonString, this, mDrive, mFolder, person.name + Fileman.JSON_EXTENSION, false, true)
+        return Fileman.write(
+            jsonString,
+            this,
+            mDrive,
+            mFolder,
+            person.name + Fileman.JSON_EXTENSION,
+            false,
+            true
+        )
     }
 
     private fun readPerson(): String? {
--- fileman/src/main/java/com/andrefilgs/fileman/Fileman.kt
+++ fileman/src/main/java/com/andrefilgs/fileman/Fileman.kt
@@ -1,8 +1,6 @@
 package com.andrefilgs.fileman
 
-import android.annotation.TargetApi
 import android.content.Context
-import android.os.Build
 import android.util.Log
 import java.io.*
 import java.nio.charset.Charset
@@ -11,23 +9,23 @@ import java.nio.charset.Charset
 class Fileman {
   companion object {
     private val TAG = "Fileman"
-
+    
     val DRIVE_SB = 0 //SandBox
     val DRIVE_SDI = 1 //Internal Device storage
     val DRIVE_SDE = 2 //External Device storage (SD card)
-
+    
     //Extensions
     val JSON_EXTENSION = ".json"
     val VIDEO_EXTENSION = ".mp4"
     val TEMP_EXTENSION = ".tmp"
     val HTML_EXTENSION = ".html"
     val THUMB_EXTENSION = ".png"
-
-
+    
+    
     // ============================== Write ==============================
-
-    @TargetApi(Build.VERSION_CODES.KITKAT)
-    fun getFullPath(context: Context, driveInt: Int, folderStr: String, showLog:Boolean? = false): String? {
+    
+    
+    private fun getFullPath(context: Context, driveInt: Int, folderStr: String, showLog: Boolean? = false): String? {
       var driveStringName = ""
       val pathStr: String
       if (driveInt == DRIVE_SB) {
@@ -37,24 +35,24 @@ class Fileman {
         if (path[0] != null) {
           driveStringName = path[0].path
         } else {
-          if(showLog!!) Log.d(TAG, "No emulator")
+          if (showLog!!) Log.d(TAG, "No emulator")
           return null
         }
       } else if (driveInt == DRIVE_SDE) {
-        val path =context.getExternalFilesDirs(null) //Access only by PC, others Apps can't
+        val path = context.getExternalFilesDirs(null) //Access only by PC, others Apps can't access here
         if (path.size >= 2 && path[1] != null) {
           driveStringName = path[1].path
         } else {
-          if(showLog!!) Log.d(TAG, "No SD card available")
+          if (showLog!!) Log.d(TAG, "No SD card available")
           return null
         }
       }
       pathStr = driveStringName + folderStr
       return pathStr
     }
-
+    
     //Check if Generic Drive Folder exists, otherwise it will be created
-    fun createFolder(context: Context, driveStr: Int, folderStr: String, showLog: Boolean? = false): File? {
+    private fun createFolder(context: Context, driveStr: Int, folderStr: String, showLog: Boolean? = false): File? {
       val pathStr = getFullPath(context, driveStr, folderStr, showLog)
       if (pathStr != null) {
         val fullPath = File(pathStr)
@@ -65,90 +63,88 @@ class Fileman {
       }
       return null
     }
-
-    fun createOutputFile( context: Context, drive: Int, folder: String,  fileName: String,  append: Boolean, showLog:Boolean? = false ): OutputStream? {
+    
+    private fun createOutputFile(context: Context, drive: Int, folder: String, fileName: String, append: Boolean, showLog: Boolean? = false): OutputStream? {
       val fullPath = createFolder(context, drive, folder, showLog)
       if (fullPath != null) {
         val file = File(fullPath, fileName)
-        try {
+        return try {
           if (!file.exists()) {
             file.createNewFile()
-            if(showLog!!) Log.d(TAG, "File created")
+            if (showLog!!) Log.d(TAG, "File created")
           } else {
-            if(showLog!!) Log.d(TAG, "File already exist")
+            if (showLog!!) Log.d(TAG, "File already exist")
           }
-          return FileOutputStream(file, append)
+          FileOutputStream(file, append)
         } catch (e: IOException) {
           e.printStackTrace()
-          return null
+          null
         }
-
+        
       }
       return null
     }
-
-
+    
+    
     // ============================== Read ==============================
-
-    //Check if Generic Drive Folder exists
-    fun checkFolder(context: Context, drive: Int, folderStr: String, showLog: Boolean? = false): Boolean {
+    
+    /**
+     * Check if Generic Drive Folder exists
+     */
+    private fun checkFolder(context: Context, drive: Int, folderStr: String, showLog: Boolean? = false): Boolean {
       val pathStr = getFullPath(context, drive, folderStr, showLog) ?: //Something wrong with the path, maybe there is no SD card
-        return false
+      return false
       val dirPath = File(pathStr)
       dirPath.mkdir()
       return dirPath.exists()
     }
-
-
-    fun getFolder(context: Context, drive: Int, folderStr: String, showLog: Boolean? = false): File? {
-      if (checkFolder(context, drive, folderStr, showLog)) {
-        val pathStr = getFullPath(context, drive, folderStr, showLog)
-        return File(pathStr!!)
-      }
-      return null
+    
+    
+    private fun getFolder(context: Context, drive: Int, folderStr: String, showLog: Boolean? = false): File? {
+      if (!checkFolder(context, drive, folderStr, showLog)) return null
+      val pathStr = getFullPath(context, drive, folderStr, showLog)
+      return File(pathStr!!)
     }
-
-    fun getInputStreamFile(context: Context, drive: Int, folder: String, fileName: String, showLog: Boolean? = false): InputStream? {
-      try {
+    
+    private fun getInputStreamFile(context: Context, drive: Int, folder: String, fileName: String, showLog: Boolean? = false): InputStream? {
+      return try {
         val file = File(getFolder(context, drive, folder, showLog), fileName)
-        return FileInputStream(file)
+        FileInputStream(file)
       } catch (ex: IOException) {
         ex.printStackTrace()
-        return null
+        null
       }
-
     }
-
-// ============================== DAO ==============================
-
+    
+    
+    
     /**
-     * @drive Sandbox / Internal / External
-     * @filename wih extension e.g. ".json"
+     * @param drive Sandbox / Internal / External
+     * @param fileName wih extension e.g. ".json"
      */
-    fun write(contentString: String, context: Context, drive: Int, folder: String, fileName: String, append: Boolean,  showLog: Boolean? = false): Boolean {
+    fun write(contentString: String, context: Context, drive: Int, folder: String, fileName: String, append: Boolean, showLog: Boolean? = false): Boolean {
       val out = createOutputFile(context, drive, folder, fileName, append, showLog) ?: return false
-
+      
       return try {
         out.write(contentString.toByteArray())
         out.close()
-        if(showLog!!) Log.d(TAG, "File $fileName write with success.\nContent is $contentString")
+        if (showLog!!) Log.d(TAG, "File $fileName write with success.\nContent is $contentString")
         true
       } catch (e: IOException) {
         e.printStackTrace()
         false
       }
     }
-
-
-
+    
+    
     /**
-     * @drive Sandbox / Internal / External
-     * @filename wih extension e.g. ".json"
+     * @param drive Sandbox / Internal / External
+     * @param fileName wih extension e.g. ".json"
      */
     fun read(context: Context, drive: Int, folder: String, fileName: String, showLog: Boolean? = false): String? {
       val inputStream: InputStream? = getInputStreamFile(context, drive, folder, fileName, showLog)
-
-      var json: String? = null
+      
+      var content: String? = null
       try {
         inputStream?.let {
           //      InputStream inputStream = context.getAssets().open("file_name.json");
@@ -157,32 +153,28 @@ class Fileman {
           inputStream.read(buffer)
           inputStream.close()
           val charset: Charset = Charsets.UTF_8
-          json = String(buffer, charset)
-          if(showLog!!) Log.d(TAG, "File $fileName read with success.\nFile content is: $json")
+          content = String(buffer, charset)
+          if (showLog!!) Log.d(TAG, "File $fileName read with success.\nFile content is: $content")
         }
       } catch (ex: Exception) {
         ex.printStackTrace()
         return null
       }
-
-      return json
+      
+      return content
     }
-
-    fun delete(context: Context, drive: Int, folder: String, fileName:String): Boolean{
-      try{
+    
+    fun delete(context: Context, drive: Int, folder: String, fileName: String): Boolean {
+      return try {
         val file = File(getFolder(context, drive, folder), fileName)
         file.delete()
-        return true
-      }catch (e: Exception){
+        true
+      } catch (e: Exception) {
         e.printStackTrace()
-        return false
+        false
       }
     }
-
-
-
-
-
-
+    
+    
   }
 }
